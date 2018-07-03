c92c39d6767c11e2e4fe3fb3f0ab5e3b65b83cbf
Joshua Martin <joshua@bluefishds.com>
2018-07-03 07:56:05 -0400

    Add password env variable back to database

--- src/db/index.js
+++ src/db/index.js
@@ -8,7 +8,7 @@ let { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 export const db = new Sequelize(
   DB_NAME,
   DB_USER,
-  null,
+  DB_PASSWORD,
   {
     host: DB_HOST,
     dialect: 'postgres',
